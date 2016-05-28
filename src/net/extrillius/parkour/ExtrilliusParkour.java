c8fffaa0ca78cca5633c3b52d1a293e5bd4a2474
TechBug2012 <mattbstanciu@gmail.com>
2016-05-28 17:01:58 -0400

    updated comments

--- src/net/extrillius/parkour/ExtrilliusParkour.java
+++ src/net/extrillius/parkour/ExtrilliusParkour.java
@@ -69,7 +69,7 @@ public class ExtrilliusParkour extends JavaPlugin implements Listener {
             }
         }
     }
-    //umm...Join Signs (I think?)
+    //signs
     @EventHandler
     public void onSignChange(SignChangeEvent event) {
         if (event.getLine(0).equalsIgnoreCase("[Join]") && getConfig().contains("maps." + event.getLine(1))) {
