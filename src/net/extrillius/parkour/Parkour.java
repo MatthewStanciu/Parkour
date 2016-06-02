a9fe1be80479a41e8ca5dec111f68fbd0a7ef6dc
TechBug2012 <mattbstanciu@gmail.com>
2016-06-02 13:26:21 -0400

    removed unused import

--- src/net/extrillius/parkour/Parkour.java
+++ src/net/extrillius/parkour/Parkour.java
@@ -5,7 +5,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
